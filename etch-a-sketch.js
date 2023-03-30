44f1da4ea3942f6d2cc5c853496629acc632e80f
Nick Davis <nickdavis71@gmail.com>
2023-03-29 18:50:47 -0600

    Added slider and label for sketch gird size.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,5 +1,16 @@
 const gridContainer = document.querySelector('#grid-container');
+const sliderContainer = document.querySelector('#slider-container');
+const gridSize = document.querySelector('#grid-size');
+const slider = document.querySelector('#myRange');
 
+const root = document.querySelector(':root');
+const rootStyles = getComputedStyle(root);
+root.style.setProperty('--rows',getSliderValue());
+root.style.setProperty('--columns',getSliderValue());
+
+slider.addEventListener('click', () => {createGridSquares(getSliderValue(), getSliderValue())});
+
+createGridSquares(16,16);
 function createGridSquares (rows, columns) {
     for(let i = 0; i < (rows*columns); i++) {
         const gridSquares = document.createElement('div');
@@ -7,9 +18,10 @@ function createGridSquares (rows, columns) {
         gridContainer.appendChild(gridSquares);
     }
 }
-createGridSquares(16,16);
+
 
 const gridSquares = document.querySelectorAll('.blank-square');
+
 gridSquares.forEach((square) => equipEventListeners(square));
 
 function colorSquare (square) {
@@ -17,10 +29,17 @@ function colorSquare (square) {
     square.classList.add('black-square');
 }
 
-
 function equipEventListeners (square) {
     square.addEventListener('mousedown', () => colorSquare)
     square.addEventListener('mouseover', (e) => {
         if (e.buttons === 1) colorSquare(square);
     })
 }
+
+function getSliderValue () {
+    const slider = document.querySelector('#myRange');
+    let sliderValue = slider.value;
+    gridSize.textContent = `${sliderValue} x ${sliderValue}`;
+    return sliderValue;
+}
+
